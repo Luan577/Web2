0f13dad105950029f136764d8aaa306304f1645b
Luan577 <dluan577@gmail.com>
2022-06-07 07:39:24 -0300

    PRONTO

--- av01.sql
+++ av01.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 4.6.6deb5ubuntu0.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 26/04/2022 às 10:47
--- Versão do servidor: 5.7.28-0ubuntu0.18.04.4
--- Versão do PHP: 7.2.24-0ubuntu0.18.04.2
+-- Tempo de geração: 07/06/2022 às 07:22
+-- Versão do servidor: 5.7.37-0ubuntu0.18.04.1
+-- Versão do PHP: 7.2.34-28+ubuntu18.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -31,14 +31,20 @@ CREATE TABLE `material` (
   `nome` varchar(255) NOT NULL,
   `quantidade` mediumint(6) UNSIGNED NOT NULL,
   `datahora` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
--- Fazendo dump de dados para tabela `material`
+-- Estrutura para tabela `usuario`
 --
 
-INSERT INTO `material` (`id`, `nome`, `quantidade`, `datahora`) VALUES
-(1, 'Luan', 2, '2022-04-29 10:46:00');
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Índices de tabelas apagadas
@@ -51,6 +57,12 @@ ALTER TABLE `material`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas apagadas
 --
 
@@ -58,7 +70,12 @@ ALTER TABLE `material`
 -- AUTO_INCREMENT de tabela `material`
 --
 ALTER TABLE `material`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
